276fa9c61e3acf32f016197e8094bdac364b2ec4
MOHAMMAD WILDAN AL-HABIBIE <wildan.muhammad1310@gmail.com>
2025-09-09 10:08:04 +0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    //property class
+    int numdoors;
+    boolean iselectric;
+
+    //constructor
+    public Car() {
+        numdoors = 6;
+        iselectric = false;  
+
+    }
+    //getter
+    //setter
+    //method
+    public Car(int i, Object falsea) {
+        //TODO Auto-generated constructor stub
+    }
+    public void print() {
+        System.out.println("Number of doors: " + numdoors);
+        System.out.println("Is electric: " + iselectric);  
+    }
+}
+  
---
+++ CarTest.java
@@ -0,0 +1,9 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car Toyota = new Car();
+        Toyota.print();
+
+        Car Lamborghini = new Car(6, false);
+        Lamborghini.print();
+    }
+}
